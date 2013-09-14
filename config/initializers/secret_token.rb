786c913cd5410bbd6b98031e4d84702c1bf792a6
Andy Holst <andy.holst85@gmail.com>
2013-09-14 16:50:58 +0200

    started an ruby rails company searcher

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CompanySearch::Application.config.secret_key_base = 'd15f45af4ae2a9bde0b63da0b0fd66a087f793d1ebe79faef7d32c9091b0e4668952df614b933255859f7a51a3269b02809b3cded8b0f82dcf9377dd987a9e46'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CompanySearch::Application.config.session_store :cookie_store, key: '_company_search_session'
