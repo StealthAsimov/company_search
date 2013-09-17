c995cc5332db7bd424d9217b48334709c6af9843
Andy Holst <andy.holst85@gmail.com>
2013-09-17 13:13:25 +0200

    updated the test cases so the the created database is tested properly and that the table is created properly

--- spec/lib/mysql_database_spec.rb
+++ spec/lib/mysql_database_spec.rb
@@ -17,7 +17,30 @@ describe MysqlDatabase do
     @db_name = "test_database"
     @client = MysqlDatabase.new_client(@db_host, @db_user, @db_pass)
     @client.query("CREATE DATABASE IF NOT EXISTS #{@db_name}")
+    @result = @client.query("SHOW DATABASES")
+    @database_exist = false
+    @result.each do |row|
+      if row['Database'] == @db_name
+        @database_exist = true
+        break
+      end
+    end
+    @database_exist.should eql true
     @client.close
+  end
+  it 'shall create table if database exist' do
+    @db_host = "127.0.0.1"
+    @db_user = "root"
+    @db_pass = "NCHNk85"
+    @db_name = "test_database"
+    @client = MysqlDatabase.new_client(@db_host, @db_user, @db_pass)
+    @client.select_db(@db_name)
+    @client.query("CREATE TABLE IF NOT EXISTS companies (identification_no varchar(30), name varchar(30))")
+    @result = @client.query("SHOW TABLES")
+    @result.each do |row|
+      row['Tables_in_' + @db_name].should eql 'companies'
+      break
+    end
   end
   it 'shall drop the database if it exist' do
     @db_host = "127.0.0.1"
@@ -26,6 +49,15 @@ describe MysqlDatabase do
     @db_name = "test_database"
     @client = MysqlDatabase.new_client(@db_host, @db_user, @db_pass)
     @client.query("DROP DATABASE IF EXISTS #{@db_name}")
+    @result = @client.query("SHOW DATABASES")
+    @database_exist = false
+    @result.each do |row|
+      if row['Database'] == @db_name
+        @database_exist = true
+        break
+      end
+    end
+    @database_exist.should eql false
     @client.close
   end
 end
